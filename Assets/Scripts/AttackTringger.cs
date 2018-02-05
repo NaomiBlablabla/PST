ad46ab2c2cc0230c85f5424b784c2a60d329628d
NaomiBlablabla <jocelynaguirre96@gmail.com>
2018-02-05 16:53:41 +0100

    Subida 2 _ Proyecto stranger dreams

---
+++ Assets/Scripts/AttackTringger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTringger : MonoBehaviour {
+
+    [SerializeField]
+    private int dmg = 10;
+
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.isTrigger != true && col.CompareTag("Enemy"))
+        {
+            col.gameObject.GetComponent<Enemy>().Damage(dmg);//SendMessageUpwards("Damage", dmg);
+
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
